93e30c9208929ec38311749c0ddc3ef559186a91
Elizabeth Tran <elizabethtran@Elizabeths-MBP.lan>
2022-02-07 13:47:07 -0800

    headshot screen

---
+++ app/src/main/java/com/example/birdsofafeather/headshot.java
@@ -0,0 +1,14 @@
+package com.example.birdsofafeather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class headshot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_headshot);
+    }
+}
