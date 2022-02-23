2369048e12b3f8ea7e16b33edda89f063fb4483b
Helen Chang <hsc004@ucsd.edu>
2022-02-22 22:21:36 -0800

    MS2, US 3: Added separate activity to view favorites

--- app/src/main/java/com/example/birdsofafeather/FavoritesActivity.java
+++ app/src/main/java/com/example/birdsofafeather/FavoritesActivity.java
@@ -1,14 +1,39 @@
 package com.example.birdsofafeather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.birdsofafeather.model.db.AppDatabase;
+import com.google.android.gms.nearby.messages.MessageListener;
 
 public class FavoritesActivity extends AppCompatActivity {
+    protected RecyclerView favoritesStudentView;
+    protected RecyclerView.LayoutManager studentsLayoutManager;
+    protected StudentsViewAdapter studentsViewAdapter;
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        db = AppDatabase.singleton(getApplicationContext());
+
+        favoritesStudentView = findViewById(R.id.favorites_recyclerview);
+        studentsLayoutManager = new LinearLayoutManager(getApplicationContext());
+        favoritesStudentView.setLayoutManager(studentsLayoutManager);
+
+        studentsViewAdapter = new StudentsViewAdapter(db.studentWithCoursesDao().getFavoritedStudents());
+        favoritesStudentView.setAdapter(studentsViewAdapter);
+    }
+
+    public void onBackHomeClicked(View view) {
+        Intent homeIntent = new Intent(this, HomeActivity.class);
+        startActivity(homeIntent);
     }
 }
