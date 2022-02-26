0fc51b0d4b0da12521cad645889020e73e63d5ed
Helen Chang <hsc004@ucsd.edu>
2022-02-26 00:10:40 -0800

    MS2, US 2: edit mock unit tests to account for courses requiring new column

--- app/src/test/java/com/example/birdsofafeather/NearbyMessagesMockTest.java
+++ app/src/test/java/com/example/birdsofafeather/NearbyMessagesMockTest.java
@@ -47,7 +47,7 @@ public class NearbyMessagesMockTest {
             pastedInfo.setText("Bill,,,\nhttps://lh3.googleusercontent.com/pw/AM-JKLXQ2ix4dg-PzLrPOS" +
                     "MOOy6M3PSUrijov9jCLXs4IGSTwN73B4kr-F6Nti_4KsiUU8LzDSGPSWNKnFdKIPqCQ2dFTRbARsW76" +
                     "pevHPBzc51nceZDZrMPmDfAYyI4XNOnPrZarGlLLUZW9wal6j-z9uA6WQ=w854-h924-no?authuser=0,,," +
-                    "\n2021,FA,CSE,210\n2022,WI,CSE,110\n2022,SP,CSE,110");
+                    "\n2021,FA,CSE,210,0\n2022,WI,CSE,110,0\n2022,SP,CSE,110,0");
 
             Button enterMockStudentInfo = activity.findViewById(R.id.add_mock_student);
             enterMockStudentInfo.callOnClick();
